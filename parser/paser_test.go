984d2c3d097df6fdd87ada2af9cc404f7db6df88
ahmadrosid <alahmadrosid@gmail.com>
2022-07-18 23:00:10 +0700

    test: add more expected test fo var statment

--- parser/paser_test.go
+++ parser/paser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"github.com/ahmadrosid/yuk/ast"
 	"github.com/ahmadrosid/yuk/lexer"
 	"testing"
 )
@@ -25,7 +26,35 @@ func TestVarStatement(t *testing.T) {
 		if len(program.Statements) != 1 {
 			t.Fatalf("program.Statements does not contain 1 statement. god=%q", len(program.Statements))
 		}
+
+		stmt := program.Statements[0]
+		if !testVarStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
 	}
+}
+
+func testVarStatement(t *testing.T, stmt ast.Statement, identifier string) bool {
+	if stmt.TokenLiteral() != "var" {
+		t.Errorf("stmt.TokenLiteral no 'var'. got-%q", stmt.TokenLiteral())
+		return false
+	}
+	varStmt, ok := stmt.(*ast.VarStatement)
+	if !ok {
+		t.Errorf("stmt not *ast.VarStatemtnt. got=%q", stmt)
+		return false
+	}
+	if varStmt.Name.Value != identifier {
+		t.Errorf("varStmt.Value not '%s'. got '%s'", identifier, varStmt.Name.Value)
+		return false
+	}
+
+	if varStmt.Name.TokenLiteral() != identifier {
+		t.Errorf("varStmt.TokenLiteral not '%s'. got '%s'", identifier, varStmt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
 }
 
 func checkParseErrors(t *testing.T, p *Parser) {
