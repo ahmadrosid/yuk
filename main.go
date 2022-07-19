f39cd9cdc9686e200cd0ec4b1ed2894bdcbd4505
ahmadrosid <alahmadrosid@gmail.com>
2022-07-19 08:37:43 +0700

    feat: add entry point at main function

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/ahmadrosid/yuk/compiler"
+	"github.com/ahmadrosid/yuk/lexer"
+	"github.com/ahmadrosid/yuk/parser"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello world!")
+	if len(os.Args) == 1 {
+		log.Fatalf("please provide file path!")
+	}
+
+	result, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lex := lexer.New(string(result))
+	par := parser.New(lex)
+	gen := compiler.New(par.ParseProgram())
+	fmt.Println(gen.Generate())
 }
